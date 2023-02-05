d49d261d8256ce4ef10d7b3e2c3007c9714fea1c
Alessandro Baldo <baldoalessandro@users.noreply.github.com>
2023-02-05 19:33:04 +0100

    Adds credits and version

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { FilePondFile } from 'filepond'
 import { FilePond } from 'react-filepond'
 import { processSubtitle } from './subtitles'
 
+import { version as APP_VERSION } from "../package.json"
 import 'filepond/dist/filepond.min.css'
 
 function App() {
@@ -26,7 +27,7 @@ function App() {
   }, [files])
 
   return (
-    <div className="h-screen max-h-screen md:max-w-2xl mx-auto flex flex-col justify-around p-2">
+    <div className="h-screen max-h-screen md:max-w-2xl mx-auto flex flex-col justify-between p-2">
       <header className="select-none my-4">
         <h1 className="text-3xl text-center tracking-wider">
           SRT<span className="text-4xl px-1">2</span>TXT
@@ -46,6 +47,13 @@ function App() {
       <div className="grow-2 p-4 my-4 overflow-auto">
         {result !== undefined ? result : null }
       </div>
+      <footer className="mb-2 mt-4 text-center text-gray-500 text-sm flex justify-center space-x-2">
+        <p>made with ☕️ by Alessandro Baldo</p>
+        <p>&mdash;</p>
+        <p>version: { APP_VERSION }</p>
+        <p>&mdash;</p>
+        <p><a href="https://github.com/baldoalessandro/srt2txt" className="underline">source</a></p>
+      </footer>
     </div>
   )
 }
